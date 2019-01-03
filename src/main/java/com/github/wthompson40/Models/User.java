8912dec829a4ea32d2e9a2ecbe760dc3efbe3e48
Will Thompson <wbthompson14@gmail.com>
2019-01-03 03:17:09 -0800

    Added basic user model for conversion from OP.GG records

---
+++ src/main/java/com/github/wthompson40/Models/User.java
@@ -0,0 +1,32 @@
+package com.github.wthompson40.Models;
+
+public class User {
+
+    private String name;
+    private int kills;
+    private int damage;
+    private double kilometers;
+
+    public User(String name, int kills, int damage, double kilometers) {
+        this.name = name;
+        this.kills = kills;
+        this.damage = damage;
+        this.kilometers = kilometers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getKills() {
+        return kills;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public double getKilometers() {
+        return kilometers;
+    }
+}
