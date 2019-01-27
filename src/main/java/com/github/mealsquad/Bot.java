23fe6aa6d942d8fad0b6b94bc2660041efd1e2af
Will Thompson <wbthompson14@gmail.com>
2019-01-27 12:54:59 -0800

    Changed update time

--- src/main/java/com/github/mealsquad/Bot.java
+++ src/main/java/com/github/mealsquad/Bot.java
@@ -37,21 +37,21 @@ public class Bot {
     private void scheduleTasks() {
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
-        final Long initialDelay = LocalDateTime.now().until(LocalDate.now().plusDays(1).atTime(12, 30),
+        final Long initialDelay = LocalDateTime.now().until(LocalDate.now().plusDays(1).atTime(13, 30),
                 ChronoUnit.MINUTES);
 
         Long delayTime = (initialDelay > TimeUnit.DAYS.toMinutes(1))
-                ? LocalDateTime.now().until(LocalDate.now().atTime(12, 30), ChronoUnit.MINUTES)
+                ? LocalDateTime.now().until(LocalDate.now().atTime(13, 30), ChronoUnit.MINUTES)
                 : initialDelay;
 
         scheduler.scheduleAtFixedRate(
                 new BoardUpdate(), delayTime, TimeUnit.DAYS.toMinutes(1), TimeUnit.MINUTES);
 
-        final Long cacheInitialDelay = LocalDateTime.now().until(LocalDate.now().plusDays(1).atTime(12, 30),
+        final Long cacheInitialDelay = LocalDateTime.now().until(LocalDate.now().plusDays(1).atTime(13, 30),
                 ChronoUnit.MINUTES);
 
         Long cacheDelayTime = (initialDelay > TimeUnit.DAYS.toMinutes(1))
-                ? LocalDateTime.now().until(LocalDate.now().atTime(12, 25), ChronoUnit.MINUTES)
+                ? LocalDateTime.now().until(LocalDate.now().atTime(13, 25), ChronoUnit.MINUTES)
                 : cacheInitialDelay;
 
         // Fill cache when bot starts
