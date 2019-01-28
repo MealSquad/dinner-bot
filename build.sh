d44f3d5830a67c3d1e1dcca44619c557b6652362
wbthompson14 <wbthompson14@gmail.com>
2019-01-27 21:45:31 -0800

    Added separate log to track updates

--- build.sh
+++ build.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+# Timestamp
+echo "*************************************"
+echo "Updating dinner-bot on $(date)"
+echo "*************************************"
+
 # Pull newest changes
+echo "Fetching newest updates"
 git pull
 
 # Clean build space
+echo "Cleaning workspace"
 ./gradlew clean
 
 # Build
+echo "Building workspace"
 ./gradlew build
 
+echo "Starting dinner-bot"
 # Run
 ./gradlew run
